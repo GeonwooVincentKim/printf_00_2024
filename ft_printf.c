78b0dc7464e5210d4f1dfba46b811ca907aa5603
Geonwoo Kim <geonwkim@c1r2s2.42tokyo.jp>
2024-04-28 15:32:12 +0900

    printf:

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 12:12:24 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/04/28 13:51:12 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/04/28 15:31:08 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,31 @@ t_ftprintf	*ft_initialize_tab(t_ftprintf *tab)
 	return (tab);
 }
 
+// str[++i] - while the string exists
+// &str[i] - print what you need
+// i + 1 - start evaluate from the character after the %
 int	ft_printf(const char	*str, ...)
 {
 	t_ftprintf	*tab;
+	int			i;
+	int			ret;
 
 	tab = (t_ftprintf *)malloc(sizeof(t_ftprintf));
 	if (!tab)
 		return (-1);
 	ft_initialize_tab(tab);
+	va_start(tab->args, str);
+	i = -1;
+	ret = 0;
+	while (str[++i])
+	{
+		if (str[i] == '%')
+			i = ft_eval_format(tab, str, i + 1);
+		else
+			ret += write(1, &str[i], 1);
+	}
+	va_end(tab->args);
+	ret += tab->total_length;
+	free(tab);
+	return (ret);
 }
